7955dd34666d05aeaacc6b0030a109f1b1057e81
bankymono <bankymono@gmail.com>
2020-05-26 11:43:14 -0700

    starting code for the movie-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Search from './components/Search'
 
 function App() {
+  const apiurl = 'http://www.omdbapi.com/?i=tt3896198&apikey=487cfebc'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Movie App</h1>
       </header>
+      <main>
+
+      </main>
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Search() {
+    return (
+        <div>
+            <section>
+                <input type='text' placeholder='Search for a movie...' />
+            </section>
+        </div>
+    )
+}
+
+export default Search
